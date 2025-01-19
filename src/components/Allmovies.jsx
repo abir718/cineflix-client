6db2d453e659658c1587ef2f113b3e45c93dfe24
abir718 <abir.hasan2003s@gmail.com>
2025-01-19 22:49:43 +0600

    search functionality added

--- src/components/Allmovies.jsx
+++ src/components/Allmovies.jsx
@@ -2,45 +2,62 @@ import { useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 
 const Allmovies = () => {
-
-  const loadMovies = useLoaderData();
+  const loadMovies = useLoaderData(); 
   const [movies, setMovies] = useState(loadMovies);
+  const [search, setSearch] = useState("");
 
-    return (
-      <div className="bg-[#1b1b1b] py-10 ">
-        <div className="flex items-center gap-3 py-10 w-[80%] mx-auto">
-        <div className="w-[10px] h-[40px] bg-[#DD003F]"></div>
-        <p className="text-white text-3xl font-bold">Add Movies</p>
+  const handleSearch = (e) => {
+    const query = e.target.value.toLowerCase(); 
+    setSearch(query); 
+
+    const filteredMovies = loadMovies.filter((movie) =>
+      movie.title.toLowerCase().includes(query) 
+    );
+    setMovies(filteredMovies);
+  };
+
+  return (
+    <div className="bg-[#1b1b1b] py-10 ">
+      <div className="w-[70%] mx-auto flex items-center justify-between">
+        <div className="flex items-center gap-3 py-10 ">
+          <div className="w-[10px] h-[40px] bg-[#DD003F]"></div>
+          <p className="text-white text-3xl font-bold">Add Movies</p>
         </div>
-        <div className=" w-[80%] mx-auto grid grid-cols-3">
-        {movies.map((movie) => (
-          <div
-            key={movie._id}
-           className="mb-10 bg-[#232323]  flex flex-col items-center justify-center w-fit rounded-lg p-6 gap-4" >
+        <div>
+          <input type="text" placeholder="Search..."  value={search} onChange={handleSearch} className="text-gray-400 input bg-[#282828] border-gray-500 input-bordered" />
+        </div>
+      </div>
+      <div className="flex justify-center items-center">
+        <div className=" grid grid-cols-3 gap-x-10">
+          {movies.map((movie) => (
+            <div
+              key={movie._id}
+              className="mb-10 bg-[#232323]  flex flex-col items-center justify-center w-fit rounded-lg p-6 gap-4" >
               <div className="flex items-center gap-8 ">
-              <div>
-              <img src={movie.poster} className="w-24 h-36 object-cover"/>
-            </div>
-            <div className="flex flex-col gap-2 text-white">
-              <p className="">Title:{movie.title} </p>
-              <p className="">Genre: {movie.genre}</p>
-              <p className="">Duration: {movie.duration} mins</p>
-              <p className="">Release Year: {movie.releaseYear}</p>
-              <p className="">Rating: {movie.rating}/5</p>
-            </div>
+                <div>
+                  <img src={movie.poster} className="w-24 h-36 object-cover" />
+                </div>
+                <div className="flex flex-col gap-2 text-white">
+                  <p className="">Title:{movie.title} </p>
+                  <p className="">Genre: {movie.genre}</p>
+                  <p className="">Duration: {movie.duration} mins</p>
+                  <p className="">Release Year: {movie.releaseYear}</p>
+                  <p className="">Rating: {movie.rating}/5</p>
+                </div>
               </div>
               <Link to={`/allmovies/${movie._id}`}><button className="text-white bg-[#DD003F] w-fit rounded-lg py-2 px-3">View Details</button></Link>
-              
 
 
-          
-          </div>
-        ))}
+
+
+            </div>
+          ))}
         </div>
-        
       </div>
-    );
-  };
-  
-  export default Allmovies;
-  
+
+
+    </div>
+  );
+};
+
+export default Allmovies;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,48 +1,62 @@
-import { useContext, useRef } from "react";
+import { useContext } from "react";
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 import { authContext } from "../Authprovider";
 
-
 const Login = () => {
-    const navigate = useNavigate()
-    let {login , setUser} = useContext(authContext)
-    let location = useLocation()
-    let emailRef = useRef(null)
-    let handleSubmit = (e) => {
-        e.preventDefault()
-        let email = emailRef.current.value
-        let password = e.target.password.value
-        login(email , password)
-        .then((result) => {
-            const user = result.user;
-            setUser(user)
-            toast.success('login successful')
-            navigate(location?.state ? location.state : "/")
-          })
-          .catch((error) => {
-            const errorMessage = error.message;
-            toast.error(`${errorMessage}`)
-          });
-    }
+    const navigate = useNavigate();
+    let { login, setUser } = useContext(authContext);
+    let location = useLocation();
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    let onSubmit = ({ email, password }) => {
+        login(email, password)
+            .then((result) => {
+                const user = result.user;
+                setUser(user);
+                toast.success("Login successful");
+                navigate(location?.state ? location.state : "/");
+            })
+            .catch((error) => {
+                const errorMessage = error.message;
+                toast.error(`${errorMessage}`);
+            });
+    };
 
     return (
         <div>
             <div className="hero bg-[#1b1b1b]">
                 <div className="card bg-[#232323] w-full max-w-sm shrink-0 shadow-2xl my-14">
-                    <form className="card-body" onSubmit={handleSubmit}>
-
+                    <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text text-white">Email</span>
                             </label>
-                            <input ref={emailRef} type="email" placeholder="email" name="email" className="text-gray-400 input bg-[#282828] border-gray-500 input-bordered" required />
+                            <input
+                                type="email"
+                                placeholder="email"
+                                className="text-gray-400 input bg-[#282828] border-gray-500 input-bordered"
+                                {...register("email", { required: "Email is required" })}
+                            />
+                            {errors.email && <p className="text-red-500 text-sm">{errors.email.message}</p>}
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text text-white">Password</span>
                             </label>
-                            <input type="password" name="password" placeholder="password" className="text-gray-400 input bg-[#282828] border-gray-500 input-bordered" required />
+                            <input
+                                type="password"
+                                placeholder="password"
+                                className="text-gray-400 input bg-[#282828] border-gray-500 input-bordered"
+                                {...register("password", { required: "Password is required" })}
+                            />
+                            {errors.password && <p className="text-red-500 text-sm">{errors.password.message}</p>}
                             <label className="label">
                                 <button className="label-text-alt link link-hover text-gray-400">Forgot password?</button>
                             </label>
@@ -51,13 +65,13 @@ const Login = () => {
                             <button className="py-2 rounded-lg text-white bg-[#DD003F] hover:scale-105 transition duration-300">Login</button>
                         </div>
                     </form>
-                    <p className="pl-4 pb-2 text-gray-400">Dont have an account? <span className="underline"><NavLink to='/register'>Register</NavLink></span></p>
+                    <p className="pl-4 pb-2 text-gray-400">
+                        Don`t have an account? <span className="underline"><NavLink to='/register'>Register</NavLink></span>
+                    </p>
                 </div>
-
             </div>
         </div>
     );
 };
 
 export default Login;
-  
