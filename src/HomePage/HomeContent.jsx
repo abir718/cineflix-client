df64fce4daf427585ca278a55234b33f4898ddbe
abir718 <abir.hasan2003s@gmail.com>
2025-04-11 23:59:32 +0600

    top movies added

--- src/HomePage/HomeContent.jsx
+++ src/HomePage/HomeContent.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import PopularHome from './PopularHome'
+import TopRated from './TopRated'
 
 function HomeContent() {
   return (
     <div>
         <PopularHome></PopularHome>
+        <TopRated></TopRated>
     </div>
   )
 }
--- src/HomePage/TopRated.jsx
+++ src/HomePage/TopRated.jsx
@@ -1,30 +1,30 @@
 import React, { useEffect, useState } from 'react'
 
-function PopularHome() {
+function TopRated() {
 
-    let [popular, setPopular] = useState([])
+    let [top, setTop] = useState([])
 
     const API_KEY = import.meta.env.VITE_API_KEY;
 
     useEffect(() => {
-        fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=${API_KEY}`)
+        fetch(`https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1&api_key=${API_KEY}`)
             .then((res) => res.json())
-            .then((data) => { setPopular(data.results) })
+            .then((data) => { setTop(data.results) })
     }, [API_KEY]);
-    console.log(popular)
+    console.log(top)
 
     return (
         <div>
             <div className="mx-auto w-[95%] flex gap-3 items-center py-6">
                 <div className="w-[10px] h-[40px] bg-[#DD003F]"></div>
-                <p className="text-3xl font-bold text-white">Popular Movies</p>
+                <p className="text-3xl font-bold text-white">Top Movies</p>
             </div>
             <div className='grid grid-cols-6 mx-auto w-[90%]'>
-                {popular.slice(0, 6).map((pop => (
-                    <div className='bg-[#232323] w-fit p-3 rounded-lg' key={pop.id}>
+                {top.slice(0, 6).map((pop => (
+                    <div className='bg-[#232323] p-3 rounded-lg' key={pop.id}>
                         <img className='w-52 rounded-lg' src={`https://image.tmdb.org/t/p/w500${pop.poster_path}`} alt={pop.original_title} />
                         <div className='flex justify-between items-center mt-2'>
-                            <h1 className='text-white text-xl'>{pop.original_title}</h1>
+                            <h1 className='text-white text-lg'>{pop.title}</h1>
                             <p className="text-sm font-medium text-gray-400">⭐ {Math.round(pop.vote_average)}/10</p>
                         </div>
 
@@ -36,4 +36,4 @@ function PopularHome() {
     )
 }
 
-export default PopularHome
+export default TopRated
