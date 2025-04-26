12719089a32ab22d88967c77a8a425f570db689e
abir718 <abir.hasan2003s@gmail.com>
2025-04-26 21:03:00 +0600

    slider issue fixed

--- src/TvPage/TvSlides.jsx
+++ src/TvPage/TvSlides.jsx
@@ -1,12 +1,58 @@
-import React from 'react'
-import BreakingBad from '../Slides/BreakingBad'
+import React from 'react';
+import BreakingBad from '../Slides/BreakingBad';
+import Sopranos from '../Slides/Sopranos';
+import GameOfThrones from '../Slides/GameOfThrones';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
+import { MdOutlineArrowBackIosNew, MdOutlineArrowForwardIos } from 'react-icons/md';
 
-function TvSlides() {
+function NextArrow(props) {
+  const { onClick } = props;
   return (
-    <div>
-        <BreakingBad></BreakingBad>
+    <div className="absolute right-0 top-1/2 transform -translate-y-1/2 text-4xl text-[#DD003F] cursor-pointer z-10" onClick={onClick}><MdOutlineArrowForwardIos />
     </div>
-  )
+  );
 }
 
-export default TvSlides
+function PrevArrow(props) {
+  const { onClick } = props;
+  return (
+    <div className="absolute left-0 top-1/2 transform -translate-y-1/2 text-4xl text-[#DD003F] cursor-pointer z-10" onClick={onClick}><MdOutlineArrowBackIosNew />
+    </div>
+  );
+}
+
+function TvSlides() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    pauseOnHover: false,
+    arrows: true,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+  };
+
+  return (
+    <div className="max-w-[1400px] mx-auto relative">
+      <Slider {...settings}>
+        <div>
+          <BreakingBad />
+        </div>
+        <div>
+          <Sopranos />
+        </div>
+        <div>
+          <GameOfThrones />
+        </div>
+      </Slider>
+    </div>
+  );
+}
+
+export default TvSlides;
